552f2b084418842f6f871f742cacb960c1362898
Daniel Valadez <danielvaladez2017@gmail.com>
2025-02-02 21:47:07 -0800

    Issue #3 - Removed some boilerplate styles/added font awesome

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./login.css"; 
+
 
 const LoginPage = () => {
+  const [userName, setUserName] = useState(''); 
+  const [password, setPassword] = useState(''); 
+
   return (
-    <div>LoginPage</div>
+    <div className="loginPage">
+      <div className="loginHeader">
+        <FontAwesomeIcon icon="fa-solid fa-heart-pulse" />
+        <h1>Workouts Built For You, By You</h1>
+      </div>
+    </div>
   )
 }
 
